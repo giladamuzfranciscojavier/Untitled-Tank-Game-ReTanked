b4e4f79fa2088c047179d77315b80642a2c36d1a
Francisco Javier Gil Adamuz <franciscojaviergilada.x2@gmail.com>
2026-05-19 23:53:38 +0200

    Estructura

---
+++ Project/Assets/Scripts/Modules/GenericModuleController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GenericModuleController : ModuleController
+{
+
+    [SerializeField] GenericModule module;
+
+    public void Setup(GenericModule module)
+    {
+        hp = maxHp = module.GetHp();
+        armor = maxArmor = module.GetArmor();
+        weight = module.GetWeight();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = module.GetSprite();
+    }
+
+    public string getDisplayName()
+    {
+        return module.GetDisplayName();
+    }
+}
